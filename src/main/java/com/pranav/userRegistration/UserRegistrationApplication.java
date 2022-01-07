4b8c68d3003dfc3dfe92ace2b40c84cdb1fff940
Pranav Saharawat <iampranav12@gmail.com>
2022-01-07 18:15:30 +0530

    Initial-Commit

---
+++ src/main/java/com/pranav/userRegistration/UserRegistrationApplication.java
@@ -0,0 +1,14 @@
+package com.pranav.userRegistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRegistrationApplication.class, args);
+		System.out.println("++++++++ User Registration App Up and Running++++++++++++++++");
+	}
+
+}
